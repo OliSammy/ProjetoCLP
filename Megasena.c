d9e308bfa4e4a60ecdfedf6c16a9d5e34b8fdde9
Samuel <samuel.cristhian@aluno.uece.br>
2022-12-08 15:31:05 -0300

    Parte final 2 kkk

--- Megasena.c
+++ Megasena.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 6 // Número de números na cartela
+
 int main() {
-  int **combinations;
+  // Criamos um arquivo para armazenar as cartelas
+  FILE *combinacoes = fopen("cartelas.txt", "w");
 
-  // Aloca memória para o vetor de combinações
-  combinations = malloc(sizeof(int*) * 500000);
-  for (int i = 0; i < 500000; i++) {
-    combinations[i] = malloc(sizeof(int) * 6);
-  }
+  // Criamos um vetor que irá armazenar os números da cartela
+  int cartelas[N];
 
-  // Gera as combinações
-  int count = 0;
-  for (int num1 = 1; num1 <= 60; num1++) {
-    for (int num2 = 1; num2 <= 60; num2++) {
-      for (int num3 = 1; num3 <= 60; num3++) {
-        for (int num4 = 1; num4 <= 60; num4++) {
-          for (int num5 = 1; num5 <= 60; num5++) {
-            for (int num6 = 1; num6 <= 60; num6++) {
-              combinations[count][0] = num1;
-              combinations[count][1] = num2;
-              combinations[count][2] = num3;
-              combinations[count][3] = num4;
-              combinations[count][4] = num5;
-              combinations[count][5] = num6;
-              count++;
+  // Geramos todas as combinações de cartelas
+  for (int a = 1; a <= 60; a++) {
+    cartelas[0] = a;
+    for (int b = a + 1; b <= 60; b++) {
+      cartelas[1] = b;
+      for (int c = b + 1; c <= 60; c++) {
+        cartelas[2] = c;
+        for (int d = c + 1; d <= 60; d++) {
+          cartelas[3] = d;
+          for (int e = d + 1; e <= 60; e++) {
+            cartelas[4] = e;
+            for (int f = e + 1; f <= 60; f++) {
+              cartelas[5] = f;
 
+              // Escrevemos a cartela atual no arquivo
+              for (int i = 0; i < N; i++) {
+                fprintf(combinacoes, "%d ", cartelas[i]);
+              }
+              fprintf(combinacoes, "\n");
             }
           }
         }
@@ -33,16 +36,8 @@ int main() {
     }
   }
 
-  // Imprime as combinações
-  for (int i = 0; i < 500000; i++) {
-    printf("%d %d %d %d %d %d\n", combinations[i][0], combinations[i][1], combinations[i][2], combinations[i][3], combinations[i][4], combinations[i][5]);
-  }
-
-  // Libera a memória alocada
-  for (int i = 0; i < 500000; i++) {
-    free(combinations[i]);
-  }
-  free(combinations);
+  // Fechamos o arquivo
+  fclose(combinacoes);
 
   return 0;
 }
